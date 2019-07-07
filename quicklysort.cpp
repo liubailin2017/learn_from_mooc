ffba0bec933673980ba4434c2db30a31d7be400b
liubailin2017 <liubailinprivate@163.com>
2019-07-07 23:51:49 +0800

    quicksort.cpp

---
+++ quicklysort.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+void swap(int &a,int &b) {
+    if(a != b) {
+        int t = a;
+        a = b;
+        b = t;
+    }
+}
+
+int div(int arr[],int l,int r) { 
+    while( l < r){
+        while(l < r && arr[l] <= arr[r]) {
+           r --; 
+        }
+        if(l < r) {
+            swap(arr[l],arr[r]); 
+            l++;
+        }
+       
+        while(l<r && arr[l] <= arr[r]) {
+            l ++;
+        }
+        if(l<r) {
+            swap(arr[l],arr[r]);
+            r--;
+        }
+    }
+    return l;
+}
+
+void sort(int arr[],int l,int r) {
+    if(l < r) {
+        int d = div(arr,l,r);
+        sort(arr,l,d-1);
+        sort(arr,d+1,r);
+    }
+}
+
+void quickly_sort(int arr[],int size) {
+    sort(arr,0,size-1);
+}
+
+int main() {
+    int a[] = {5,6,5,7,4,5,1,6,5,4};
+    //int a[] = {4,5,5,7,4,5,1,6,5,4};
+    quickly_sort(a,10);
+   //std::cout<<div(a,0,9)<<std::endl;
+    for(int i = 0; i < 10; i++) {
+        std::cout<<a[i] << " " ;
+    }
+    std::cout<<std::endl;
+    return 0;
+}
